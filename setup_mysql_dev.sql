fa3f4de453cdb95cde84689604cbd2223e3d1529
Gzoref <gzoref@gmail.com>
2020-03-29 19:11:06 -0400

    prepares a MySQL server for the project

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+-- Prepares a MySQL server for the project
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performace_schema .* TO 'hbnb_dev'@'localhost';
+GRANT ALL PRIVILEGES ON hbnb_dev_db .* TO 'hbnb_dev'@'localhost';
