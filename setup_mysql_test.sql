5058c5c2a71d461cb7fdb7fc631e112c6fb91de5
Mitchell <mmoscovics@yahoo.com>
2020-03-31 00:49:03 +0000

    [Bugfix] spelling error

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -5,5 +5,5 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 -- Add privileges
 GRANT USAGE ON * . * TO 'hbnb_test'@'localhost';
-GRANT SELECT ON preformance_schema . * TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema . * TO 'hbnb_test'@'localhost';
 GRANT ALL PRIVILEGES ON hbnb_test_db . * TO 'hbnb_test'@'localhost';
