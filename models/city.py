e39272f66b37e39f9ebb249eb6ca52b87f687330
Mitchell <mmoscovics@yahoo.com>
2020-03-31 22:12:23 +0000

    [Bugfix] typo

--- models/city.py
+++ models/city.py
@@ -11,7 +11,7 @@ class City(BaseModel, Base):
         state_id: The state id
         name: input name
     """
-    __tablename__ = 'city'
+    __tablename__ = 'cities'
 
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
