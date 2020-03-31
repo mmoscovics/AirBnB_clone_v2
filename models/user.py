129b6b1909d46a2ac6aa78ed027354cf9a60b3f3
Gzoref <gzoref@gmail.com>
2020-03-31 15:59:40 -0400

    Update user to orm

--- models/user.py
+++ models/user.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
 """This is the user class"""
 from models.base_model import BaseModel
+from os import getenv
+from models.base_model import BaseModel, Base
+from models.city import City
+import models
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
-class User(BaseModel):
+
+class User(BaseModel, Base):
     """This is the class for user
     Attributes:
         email: email address
@@ -11,7 +19,11 @@ class User(BaseModel):
         first_name: first name
         last_name: last name
     """
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+
+    __tablename__ = 'users'
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
