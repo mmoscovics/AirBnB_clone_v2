93f0aae2382284d876fc78c7b69b59c5f1a47cbf
Gzoref <gzoref@gmail.com>
2020-04-01 19:52:14 -0400

    Add if statements

--- models/place.py
+++ models/place.py
@@ -40,44 +40,46 @@ class Place(BaseModel, Base):
 
     __tablename__ = 'places'
 
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
-    name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0)
-    number_bathrooms =  Column(Integer, nullable=False, default=0)
-    max_guest =  Column(Integer, nullable=False, default=0)
-    price_by_night =  Column(Integer, nullable=False, default=0)
-    latitude =  Column(Float)
-    longitude =  Column(Float)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        name = Column(String(128), nullable=False)
+        description = Column(String(1024), nullable=True)
+        number_rooms = Column(Integer, nullable=False, default=0)
+        number_bathrooms =  Column(Integer, nullable=False, default=0)
+        max_guest =  Column(Integer, nullable=False, default=0)
+        price_by_night =  Column(Integer, nullable=False, default=0)
+        latitude =  Column(Float)
+        longitude =  Column(Float)
 
-    reviews = relationship('Review', backref='place', cascade='all, delete')
-    amenities = relationship('Amenity', secondary=place_amenity,
-                                    backref='place_amenities', viewonly=False)
-    amenity_ids = []
+        reviews = relationship('Review', backref='place', cascade='all, delete')
+        amenities = relationship('Amenity', secondary=place_amenity,
+                                        backref='place_amenities', viewonly=False)
+        amenity_ids = []
 
-    @property
-    def reviews(self):
-        """returns a list of review instances with place_id"""
-        review_list = []
-        review_dict = models.storage.all(Review)
-        for key, value in review_dict.items():
-                review_list.append(review_dict[key])
-        return review_list
+    else:
+        @property
+        def reviews(self):
+            """returns a list of review instances with place_id"""
+            review_list = []
+            review_dict = models.storage.all(Review)
+            for key, value in review_dict.items():
+                    review_list.append(review_dict[key])
+            return review_list
 
-    @property
-    def amenities(self):
-        """
-        Returns the list of Amenity instances 
-        based on the attribute amenity_ids
-        """
-        return self.amenity_ids
+        @property
+        def amenities(self):
+            """
+            Returns the list of Amenity instances 
+            based on the attribute amenity_ids
+            """
+            return self.amenity_ids
 
-    @amenities.setter
-    def amenities(self, obj):
-        """
-        Add id to amenity_ids
-        """
-        for key, value in models.storage.all(Amenity):
-                if value.amenity_id == amenity.id:
-                    self.amenity_ids.append(value)
+        @amenities.setter
+        def amenities(self, obj):
+            """
+            Add id to amenity_ids
+            """
+            for key, value in models.storage.all(Amenity):
+                    if value.amenity_id == amenity.id:
+                        self.amenity_ids.append(value)
