7574a6fc525800c8240f6466213896dcbad4fb7e
Mitchell <mmoscovics@yahoo.com>
2020-04-02 21:33:36 +0000

    [Bugfix] check env

--- models/state.py
+++ models/state.py
@@ -15,19 +15,20 @@ class State(BaseModel, Base):
     """
     __tablename__ = 'states'
 
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='all, delete',
-                            backref='state')
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', cascade='all, delete',
+                              backref='state')
+    else:
+        @property
+        def cities(self):
+            """
+            Returns the list of City instances with
+            state_id equals to the current State.id
+            """
+            city_list = []
 
-    @property
-    def cities(self):
-        """
-        Returns the list of City instances with
-        state_id equals to the current State.id
-        """
-        city_list = []
-
-        for key, value in models.storage.all(State):
-                if value.state_id == self.id:
-                    city_list[key] = value
-        return city_list
+            for key, value in models.storage.all(State):
+                    if value.state_id == self.id:
+                        city_list[key] = value
+            return city_list
