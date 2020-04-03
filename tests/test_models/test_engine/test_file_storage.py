5d8b6413ecc74e021bfe20df54ad54baff3625a1
Gzoref <gzoref@gmail.com>
2020-04-02 21:46:53 -0400

    storage type test

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -64,6 +64,7 @@ class TestFileStorage(unittest.TestCase):
         key = user.__class__.__name__ + "." + str(user.id)
         self.assertIsNotNone(obj[key])
 
+    @unittest.skipIf(os.getenv("HBNB_TYPE_STORAGE") == 'db', 'Wrong engine')
     def test_reload_filestorage(self):
         """
         tests reload
