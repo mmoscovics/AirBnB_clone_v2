b018b854f732e94b56eb74a52c1bcfe210d7816d
Gzoref <gzoref@gmail.com>
2020-04-02 20:32:04 -0400

    db_storage unit test

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -52,3 +52,6 @@ class TestDatabaseStorage(unittest.TestCase):
         result = pep8style.check_files(['models/engine/db_storage.py'])
         self.assertEqual(result.total_errors, 0,
                          'Found pep8 style errors (and warnings)')
+
+if __name__ == "__main__":
+    unittest.main()
