ca5e1200a376068e086a991a6ca74e6441ba7c10
Gzoref <gzoref@gmail.com>
2020-04-02 20:29:43 -0400

    db_storage unit test

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -18,6 +18,34 @@ from models.review import Review
 class TestDatabaseStorage(unittest.TestCase):
     '''this will test the database'''
 
+    @classmethod
+    def setUpClass(cls):
+        """
+        set up for test
+        """
+        cls.user = User()
+        cls.user.first_name = "Kev"
+        cls.user.last_name = "Yo"
+        cls.user.email = "1234@yahoo.com"
+        cls.storage = DBStorage()
+
+    @classmethod
+    def teardown(cls):
+        """at the end of the test this will tear it down."""
+        del cls.user
+
+    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
+                     'db engine not being used')
+    def test_all(self):
+        """tests all in File Storage"""
+        storage = DBStorage()
+        storage.reload()
+        obj_length = len(storage.all())
+        state = State(name="Connecticut")
+        state.save()
+        storage.save()
+        self.assertsIs(len(storage.all()), obj_length + 1)
+
     def test_pep8_dbstorage(self):
         """Testing the pep8 linter requirments."""
         pep8style = pep8.StyleGuide(quiet=True)
