cb57fbff92c2926d1f42a41396c500ae32c61783
Gzoref <gzoref@gmail.com>
2020-04-02 20:21:02 -0400

    db_storage unit test

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -18,37 +18,9 @@ from models.review import Review
 class TestDatabaseStorage(unittest.TestCase):
     '''this will test the database'''
 
-    @classmethod
-    def setUpClass(cls):
-        """
-        set up for test
-        """
-        cls.user = User()
-        cls.user.first_name = "Kev"
-        cls.user.last_name = "Yo"
-        cls.user.email = "1234@yahoo.com"
-        cls.storage = DBStorage()
-
-    @classmethod
-    def teardown(cls):
-        """at the end of the test this will tear it down."""
-        del cls.user
-
-    @unittest.skipIf(os.getenv('HBNB_TYPE_STORAGE') != 'db',
-                     'db engine not being used')
-    def test_all(self):
-        """tests all in File Storage"""
-        storage = DBStorage()
-        storage.reload()
-        obj_length = len(storage.all())
-        state = State(name="Connecticut")
-        state.save()
-        storage.save()
-        self.assertsIs(len(storage.all()), obj_length + 1)
-
     def test_pep8_dbstorage(self):
         """Testing the pep8 linter requirments."""
         pep8style = pep8.StyleGuide(quiet=True)
         result = pep8style.check_files(['models/engine/db_storage.py'])
         self.assertEqual(result.total_errors, 0,
-                         'Found pep8 style errors')
+                         'Found pep8 style errors (and warnings)')
