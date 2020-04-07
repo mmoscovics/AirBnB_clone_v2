db3db788423ae195b6463a2c2f8f7ea70557578d
Mitchell <mmoscovics@yahoo.com>
2020-04-07 02:05:26 +0000

    [Bugfix] typo

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,6 +7,6 @@ sudo mkdir -p /data/web_static/shared/
 echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
-sudo sed -i "26i \ \tlocation /hbnb_static {\n\t\talias /data/web/web_static/current;\n\t}" /etc/nginx/sites-available/default
+sudo sed -i "26i \ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}" /etc/nginx/sites-available/default
 sudo service nginx reload
 sudo service nginx restart
