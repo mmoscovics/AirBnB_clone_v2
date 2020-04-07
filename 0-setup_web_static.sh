dbff43f5dc367d149a57d2a5526aa1c3a4c2b3a6
Mitchell <mmoscovics@yahoo.com>
2020-04-07 01:23:16 +0000

    [Create] task 0 prepare for web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+# Sets up web servers for deployment of web_static
+sudo apt-get -y update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir /data/web_static/shared/
+echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data
+sudo sed -i "26i \ \tlocation /hbnb_static {\n\t\talias /data/web/web_static/current;\n\t}" /etc/nginx/sites-available/default
+sudo service nginx restart
