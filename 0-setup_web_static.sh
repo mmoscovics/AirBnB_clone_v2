33934f6d40d22b48d604e0fd5835ac2150be5586
Mitchell <mmoscovics@yahoo.com>
2020-04-07 02:00:40 +0000

    [Update] create directory, add reload

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -3,9 +3,10 @@
 sudo apt-get -y update
 sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir /data/web_static/shared/
+sudo mkdir -p /data/web_static/shared/
 echo "Holberton School" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 sudo sed -i "26i \ \tlocation /hbnb_static {\n\t\talias /data/web/web_static/current;\n\t}" /etc/nginx/sites-available/default
+sudo service nginx reload
 sudo service nginx restart
