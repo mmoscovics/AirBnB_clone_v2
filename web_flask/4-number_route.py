b8ae1b6e15326bdd1a5a49f664e89a82a05a6510
Mitchell <mmoscovics@yahoo.com>
2020-04-21 20:13:37 +0000

    [Create] task 4 number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """Displays Hello HBNB!"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb')
+def hbnb():
+    """Displays HBNB"""
+    return "HBNB"
+
+
+@app.route('/c/<text>')
+def c_text(text):
+    """Displays C followed by text"""
+    return "C {}".format(text.replace('_', ' '))
+
+
+@app.route('/python/')
+@app.route('/python/<text>')
+def python_text(text='is cool'):
+    """Displays Python is cool or Python followed by text"""
+    return "Python {}".format(text.replace('_', ' '))
+
+
+@app.route('/number/<n>')
+def number(n):
+    """Displays integer followed by is a number"""
+    return "{} is a number".format(int(n))
+
+if __name__ == "__main__":
+    app.url_map.strict_slashes = False
+    app.run(host='0.0.0.0')
